4d49f37670c37ba723c91522de4e70d22a89e895
inazumark <inazumarkmail@gmail.com>
2021-11-24 23:23:21 +0100

    redesigned dataset window

--- UI/dataset_window.py
+++ UI/dataset_window.py
@@ -1,5 +1,5 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QStackedWidget, QGraphicsDropShadowEffect
+from PyQt5.QtCore import QEvent
+from PyQt5.QtWidgets import QMainWindow, QStackedWidget
 from PyQt5.uic import loadUi
 
 
@@ -7,19 +7,39 @@ class DatasetWindow(QMainWindow):
     def __init__(self, stackedWidget: QStackedWidget):
         super(DatasetWindow, self).__init__()
         loadUi('UI/Ui/dataset.ui', self)
-        self.stackedWidget = stackedWidget
-        self.file_name = None
-        self.nextButton.clicked.connect(self.next)
-        self.browseButton.clicked.connect(self.browse)
-        self.breastCancerWidget.mouseReleaseEvent = self.select
+
+        global datasets
+        datasets = [self.widget_datasetA, self.widget_datasetB, self.widget_datasetC]
+
+        # Dataset Selection
+        for dataset in datasets:
+            dataset.installEventFilter(self)
+
+        # Footer Navigation Buttons
+        self.pushButton_exit.clicked.connect(self.close)
+        self.pushButton_back.clicked.connect(self.back)
+        self.pushButton_next.clicked.connect(self.next)
+
+    def eventFilter(self, source, event):
+        if source.property("dataset") is not None:
+            if event.type() == QEvent.MouseButtonPress:
+                # Deselect all datasets
+                for dataset in datasets:
+                    if dataset.isEnabled() is False:
+                        continue
+                    dataset.setProperty("selected", False)
+                    dataset.setStyleSheet(dataset.styleSheet())
+
+                # Select clicked dataset
+                source.setProperty("selected", True)
+                source.setStyleSheet(source.styleSheet())
+        elif source.property("href-text") is True:
+            self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
+
+        return super(DatasetWindow, self).eventFilter(source, event)
+
+    def back(self):
+        print("back")
 
     def next(self):
-        self.stackedWidget.setCurrentIndex(self.stackedWidget.currentIndex() + 1)
-
-    def browse(self):
-        self.file_name = QFileDialog.getOpenFileName(self, 'Open file', './')[0]
-        self.pathLine.setText(self.file_name)
-
-    def select(self, event):
-        shadow = QGraphicsDropShadowEffect(blurRadius=5, xOffset=3, yOffset=3)
-        self.breastCancerWidget.setGraphicsEffect(shadow)
+        print("next")
